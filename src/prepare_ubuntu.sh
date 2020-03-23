8d4619ae855219402d7e223516a1247c31c85e93
Bérénice Batut <bebatut@gmail.com>
2020-03-23 18:11:59 +0100

    Add script to prepare ubuntu vm

---
+++ src/prepare_ubuntu.sh
@@ -0,0 +1,12 @@
+## Conda
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+bash Miniconda3-latest-Linux-x86_64.sh
+
+## Aevol dependencies
+#sudo apt-get install autotools-dev
+#sudo apt-get install automake
+sudo apt-get install build-essential
+sudo apt-get install build-essential
+sudo apt-get install zlib1g-dev
+sudo apt-get install libboost-filesystem-dev
+sudo apt-get install libx11-dev
