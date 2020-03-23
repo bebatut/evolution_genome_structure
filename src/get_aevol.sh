5e0a528dc51bb9bee4f941e3116ecf6a79c5c0b9
Bérénice Batut <bebatut@gmail.com>
2020-03-23 18:09:03 +0100

    Change aevol origin

--- src/get_aevol.sh
+++ src/get_aevol.sh
@@ -1,9 +1,9 @@
 cd src
-git clone git@gitlab.inria.fr:beslon/aevol_ltisee.git
-cd aevol_ltisee
-git checkout tags/v1.0.1
+git clone git@gitlab.inria.fr:aevol/aevol.git
+cd aevol
+git checkout aevol_6
 rm -rf examples
 rm -rf .git
 cd ../
-tar -zcvf aevol_ltisee.tar.gz aevol_ltisee/
-rm -rf aevol_ltisee/
+tar -zcvf aevol.tar.gz aevol/
+rm -rf aevol/
