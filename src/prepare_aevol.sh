078ff0a0d3d19be746de8f1941b3f2f722bf8445
Bérénice Batut <bebatut@gmail.com>
2020-03-30 15:35:04 +0200

    Use cmake to prepare aevol

--- src/prepare_aevol.sh
+++ src/prepare_aevol.sh
@@ -1,8 +1,5 @@
 cd src
 tar -zxvf aevol.tar.gz
 cd aevol
-aclocal
-automake
-autoconf
-./configure --without-x
+cmake .
 make
--- src/prepare_ubuntu.sh
+++ src/prepare_ubuntu.sh
@@ -5,6 +5,7 @@ bash Miniconda3-latest-Linux-x86_64.sh
 ## Aevol dependencies
 #sudo apt-get install autotools-dev
 #sudo apt-get install automake
+sudo apt-get install cmake
 sudo apt-get install build-essential
 sudo apt-get install build-essential
 sudo apt-get install zlib1g-dev
